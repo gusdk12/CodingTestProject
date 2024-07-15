97489f8008d3ad0ad775c683e90213a426bb2d1c
gusdk12 <hyuna3384@naver.com>
2024-07-15 17:08:43 +0900

    [신현아] Programmers 달리기 경주

---
+++ CodingTestSolutions/src/basics/Programmers_Race.java
@@ -0,0 +1,30 @@
+package basics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Programmers_Race {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> player = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++){
+            // key : 선수 이름, value : 순위
+            player.put(players[i], i);
+        }
+
+        for(String calling : callings) {
+            // 이름 불린 선수 기존 순위
+            int rank = player.get(calling);
+
+            // 순위 변동
+            player.put(players[rank - 1], rank);
+            player.put(players[rank], rank - 1);
+
+            // 순위 변동 선수명
+            String name = players[rank - 1];
+            players[rank - 1] = players[rank];
+            players[rank] = name;
+        }
+        return players;
+    }
+}
