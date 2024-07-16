46300ed1f7d250b00f2e9d33349040d8b190f50f
gusdk12 <hyuna3384@naver.com>
2024-07-16 17:46:40 +0900

    [신현아] 백준 2164

---
+++ CodingTestSolutions/src/basics/Baekjoon2164.java
@@ -0,0 +1,29 @@
+package basics;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Baekjoon2164 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> cards = new LinkedList<>();
+
+        // 카드 N장 만들기
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++){
+            cards.offer(i);
+        }
+
+        while (cards.size() > 1){
+            // 맨 윗장 카드 버리기
+            cards.poll();
+            // 그 다음 맨 윗장 카드 맨뒤로 추가하기
+            cards.offer(cards.poll());
+        }
+        // 마지막 남은 카드 출력
+        System.out.println(cards.poll());
+    }
+}
+
