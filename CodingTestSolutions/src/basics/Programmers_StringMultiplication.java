9d266361da93fbd7abeca85a0dbf0300498b644c
gusdk12 <hyuna3384@naver.com>
2024-07-19 12:02:09 +0900

    [신현아] Programmers 문자열 곱하기

---
+++ CodingTestSolutions/src/basics/Programmers_StringMultiplication.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Programmers_StringMultiplication {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        answer = my_string.repeat(k);
+        return answer;
+    }
+
+}
