207718cf87de060e26ed14d3cf58241b7436d762
gusdk12 <hyuna3384@naver.com>
2024-07-27 09:50:24 +0900

    [신현아] 백준 2075

--- CodingTestSolutions/src/basics/Baekjoon2075.java
+++ CodingTestSolutions/src/basics/Baekjoon2075.java
@@ -8,18 +8,21 @@ public class Baekjoon2075 {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        PriorityQueue<Integer> num = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> num = new PriorityQueue<>(Collections.reverseOrder());
 
+        // N * N 만큼 숫자 입력
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 num.offer(sc.nextInt());
             }
         }
-        System.out.println(num);
 
-        for (int i = 0; i < num.size(); i++){
+        // 맨 앞부터 N 전까지 큰 숫자 버리기
+        for (int i = 0; i < N - 1; i++){
             num.poll();
         }
-        num.peek();
+        System.out.println(num.peek()); // N번째 숫자 출력
+
+        sc.close();
     }
 }
