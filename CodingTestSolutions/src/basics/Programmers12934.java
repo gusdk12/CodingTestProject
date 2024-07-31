f370d661d90bd39a85196d785133d26283feb358
gusdk12 <hyuna3384@naver.com>
2024-07-31 12:36:08 +0900

    [신현아] Programmers 정수 제곱근 판별

---
+++ CodingTestSolutions/src/basics/Programmers12934.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Programmers12934 {
+    public long solution(long n) {
+        if(Math.pow((long)Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        } return -1;
+
+    }
+}
