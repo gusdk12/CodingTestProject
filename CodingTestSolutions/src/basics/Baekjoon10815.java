7a8fd84f29fa525c98c427db4d2cf701a8bc5914
gusdk12 <hyuna3384@naver.com>
2024-10-08 22:03:54 +0900

    [신현아] 백준 10815

---
+++ CodingTestSolutions/src/basics/Baekjoon10815.java
@@ -0,0 +1,40 @@
+package basics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 오름차순 정렬
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            // 이진탐색
+            int result = Arrays.binarySearch(arr, num);
+
+            if (result < 0) {
+                sb.append(0 + " ");
+            } else {
+                sb.append(1 + " ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
